6fc2b38f187b89af54ed085500c0e5a4934e6fc0
Stas <sizovstanislav10@gmail.com>
2023-05-18 15:28:35 +0700

    m3 t4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 99;
+        char charH = 104;
+        char charA = 97;
+        char charR = 114;
+        System.out.print(charC);
+        System.out.print(charH);
+        System.out.print(charA);
+        System.out.print(charR);
+
     }
 }
